2e51c32d5a18641650dfb6b433fb3b837a042723
Alexander <Alexan2405@gmail.com>
2015-02-23 23:15:41 +0100

    ny endring

--- Unityfiler/Assets/Scripts/Player/Spiller.cs
+++ Unityfiler/Assets/Scripts/Player/Spiller.cs
@@ -5,27 +5,6 @@ public class Spiller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var speed = 10f;
-<<<<<<< HEAD
-
-		int trololo = 5;
-<<<<<<< HEAD
-
-	
-		int test = 1;
-		int test3 = 5;
-		int test2 = 1;
-
-		int ley = 2;
-
-		int hey = 1;
-
-		int shave = 2;
-=======
-		int det = 1;
->>>>>>> 9a948131c6f7aa6b2fdaf1e388ff0a2c783e03fe
-=======
-		int lol = 5;
->>>>>>> e5d9104a758cb5ef7ae9ef43bce3e08b3f8fbb51
 		if (Input.GetKey(KeyCode.LeftArrow)) {
 			rigidbody2D.velocity = new Vector2(speed*-1,rigidbody2D.velocity.y);
 		} 
