09fe3abfdaccb66673bf6d75ac040c6e0bf8605d
Alexander <Alexan2405@gmail.com>
2015-03-03 20:32:58 +0100

    endret litt på boksene, skal legge til lyder

--- Unityfiler/Assets/Scripts/Game/test.cs
+++ Unityfiler/Assets/Scripts/Game/test.cs
@@ -2,29 +2,43 @@
 using System.Collections;
 
 public class test : MonoBehaviour {
-
 	// Use this for initialization
+public bool blue = false;
+public bool green = false;
+public bool yellow = false;
+public bool red = false;
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log (blue);
 	}
 	void OnCollisionEnter2D (Collision2D Info) {
 		if (Info.collider.tag == "Ball") {
 			if(gameObject.tag == "Blue"){
-				Debug.Log("blue");
+				blue = true;
+				Debug.Log(blue);
 			}
 			else if(gameObject.tag == "Green"){
-				Debug.Log("green");
+				green = true;
+				Debug.Log(green);
 			}
 			else if(gameObject.tag == "Yellow"){
-				Debug.Log("yellow");
+				yellow = true;
+				Debug.Log(yellow);
 			}
 			else if(gameObject.tag == "Red"){
-				Debug.Log("red");
+				red = true;
+				Debug.Log(red);
+			}
+			if(blue == true){
+				blue = false;
+				green = false;
+				red = false;
+				yellow = false;
+				Debug.Log("tripple");
 			}
 			Destroy(gameObject);
 		}
