06c326328d773e671c37f93b6b63fd7e0206681b
Alexander <Alexan2405@gmail.com>
2015-03-03 22:00:15 +0100

    endret litt her og der

--- Unityfiler/Assets/Scripts/Effekter/Lyd.cs
+++ Unityfiler/Assets/Scripts/Effekter/Lyd.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test : MonoBehaviour {
+public class Lyd : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
-	}
 	
+	}
+
 	// Update is called once per frame
 	void Update () {
 	}
 	void OnCollisionEnter2D (Collision2D Info) {
-		if (Info.collider.tag == "Ball") {
-			Destroy (gameObject);
+		if (Info.collider.tag == "Ball"&&	gameObject.tag == "Blue") {
+			Debug.Log("blue+1");
 		}
 	}
 }
