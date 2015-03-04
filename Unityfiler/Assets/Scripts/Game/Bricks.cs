4450f9e424845fea9f2aed56eda0284efd01ecb6
Kevin Aslan <kevin_aslan@hotmail.com>
2015-03-04 02:26:51 +0100

    la til effekter

---
+++ Unityfiler/Assets/Scripts/Game/Bricks.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bricks : MonoBehaviour {
+
+
+	public GameObject Hitmarker;
+	
+	void OnCollisionEnter2D (Collision2D Info) {
+		if (Info.collider.tag == "Ball") {
+			Destroy(gameObject);
+			Instantiate(Hitmarker, transform.position, Quaternion.identity);
+			GM.instance.DestroyBrick();
+		}
+	}
+}
---
+++ Unityfiler/Assets/Scripts/Game/GM.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour {
+	
+	public int lives = 3;
+	public int bricks = 68;
+	public Text livesText;
+	public static GM instance = null;
+	public GameObject Hitmarker;
+	public GameObject LivesText;
+	// Use this for initialization
+	void Start () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	public void LoseLife() {
+		lives--;
+	}
+	
+	public void DestroyBrick() {
+		bricks--;
+	}
+}
